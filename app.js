0aa686c9ec2f15815b1a70683da57ff0e74bb0e5
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-09-04 12:30:38 -0700

    File clean up

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 let total = 0;
 let taxTotal = 0;
 
-
 function purchasedItem(str) {
-  itemName = str.replace(/[0-9.]/g, '')
+  itemName = str.replace(/[0-9.]/g, '');
   itemName = itemName.replace('at', '');
   return(itemName)
 }
 
 function salesTax(str) {
   arr = str.split('at');
-  cost = arr[1]
+  cost = arr[1];
   let salesTax = (cost * 0.15).toFixed(2);
-
-  roundCentsUp = salesTax.split('.')[1]
-
-
-  // roundUp(parseFloat(roundCentsUp * .01))
+  roundCentsUp = salesTax.split('.')[1];
   let salesTaxRounded = roundUp(parseFloat(roundCentsUp))
-
 
   add(salesTaxRounded);
   addTax(salesTaxRounded);
-  return salesTaxRounded;
+  return(salesTaxRounded);
 }
 
 function roundUp(roundCentsUp) {
@@ -31,58 +25,36 @@ function roundUp(roundCentsUp) {
 
 function itemCost(str) {
   arr = str.split('at');
-  cost = arr[1]
-  console.log(cost)
-  add(cost)
-  return cost;
-}
+  cost = arr[1];
 
-function quantity(str){
-  arr = str.split(" ");
-  let qty = arr.shift()
+  add(cost);
+  return(cost);
 }
 
 function add(cost){
   total = parseFloat(cost) + parseFloat(total);
-  
-  return total.toFixed(2);
+  return(total.toFixed(2));
 }
 
 function addTax(tax) {
   taxTotal = parseFloat(tax) + parseFloat(taxTotal);
-  return taxTotal.toFixed(2);
-  
+  return(taxTotal.toFixed(2));
 }
 
-
 document.getElementById("submitText").addEventListener("click", function () {
-  //input = parseInt(document.getElementById("inputText").value);
   input = document.getElementById("inputText").value;
   document.getElementById("items").innerHTML = purchasedItem(input);
   document.getElementById("salesTaxes").innerHTML = salesTax(input);
-  //document.getElementById("total").innerHTML = x(input);
 });
 
-// <!-- input 
-// 1 Music CD at 14.99 
-// 1 Book at 12.49
-
-// Output
-// Music CD: 16.49
-// Sales Taxes: 1.50
-// Total: 16.49 -->
-
-
 function Basket(itemInput) {
-
-  this.item = purchasedItem(itemInput)
-  this.tax = salesTax(itemInput)
-  this.price = itemCost(itemInput)
-  this.total = add(0)
-  this.taxTotal = addTax(0)
+  this.item = purchasedItem(itemInput);
+  this.tax = salesTax(itemInput);
+  this.price = itemCost(itemInput);
+  this.total = add(0);
+  this.taxTotal = addTax(0);
 }
 
-//UI Constructor
 function UI() {}
 UI.prototype.addBasketToList = function(basket) {
   const list = document.getElementById('basket-list');
@@ -93,15 +65,13 @@ UI.prototype.addBasketToList = function(basket) {
     <td>${basket.tax}</td>
   `;
   list.appendChild(row);
-  const itemsTax = document.getElementById('basket-list-tax') //
+  const itemsTax = document.getElementById('basket-list-tax')
   itemsTax.innerText = `Tax ${basket.taxTotal}`
-  const itemsTotal = document.getElementById('basket-list-total') //
+  const itemsTotal = document.getElementById('basket-list-total')
   itemsTotal.innerText = `Total: ${basket.total}`
 }
 
-// Shows alert when one of the fields is left blank
 UI.prototype.showAlert = function(themessage, className) {
-  //Constructs Element for the error message
   let div = document.createElement('div');
   div.className = `alert ${className}`;
   div.appendChild(document.createTextNode(themessage));
@@ -113,40 +83,23 @@ UI.prototype.showAlert = function(themessage, className) {
   }, 3000);
 }
 
-
-
-//Clears field method
 UI.prototype.clearfields = function(){
   document.getElementById('item').value = '';
-
 }
-
 
 document.getElementById('basket-form').addEventListener('submit', 
   function(e) {
-
-
     let item = document.getElementById('item').value
+    let basket = new Basket(item);
+    const ui = new UI();
 
-   //Creates basket
-   let basket = new Basket(item);
-   //Creates a UI object
-   const ui = new UI();
-   console.log(ui);
-   // Uncomment to see the dynamically added classes we have added as prototypes
-   
-   //Validate that the forms have content
-   if(item === '') {
-     ui.showAlert('Please fill in the field', 'error');
-   } else {
-    ui.addBasketToList(basket);
-    ui.showAlert('Item Added', 'success');
+    if(item === '') {
+      ui.showAlert('Please fill in the field', 'error');
+    } else {
+      ui.addBasketToList(basket);
+      ui.showAlert('Item Added', 'success');
+      ui.clearfields();
+    }
 
-    //Clears input fields
-    ui.clearfields();
-
-   }
-
-//prevents initial behavior
   e.preventDefault();
 });
